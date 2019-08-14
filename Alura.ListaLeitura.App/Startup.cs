fe2bcbfcbc0039a64410be2f03305f5444dbbb4f
Joao Pedro <dp.joaopedro@outlook.com>
2019-08-14 11:10:02 -0300

    Novas rotas adicionadas: -Cadastro de livro -Detalhe de livro

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -1,19 +1,61 @@
-﻿using Alura.ListaLeitura.App.Repositorio;
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public void ConfigureServices(IServiceCollection services)
         {
-            app.Run(Roteamento);
+            services.AddRouting();
         }
 
-        //teste 2
+        public void Configure(IApplicationBuilder app)
+        {
+            var builder = new RouteBuilder(app);
+            builder.MapRoute("Livros/ParaLer", LivrosParaLer);
+            builder.MapRoute("Livros/Lendo", LivrosLendo);
+            builder.MapRoute("Livros/Lido", LivrosLidos);
+            builder.MapRoute("Cadastro/NovoLivro/{nome}/{autor}", CadastroNovoLivro);
+            builder.MapRoute("Livros/Detalhes/{id:int}", DetalhesDoLivro);
+
+            var rotas = builder.Build();
+
+            app.UseRouter(rotas);
+
+            //app.Run(Roteamento);
+        }
+
+        private Task DetalhesDoLivro(HttpContext context)
+        {
+            int id = Convert.ToInt32(context.GetRouteValue("id"));
+            var _repo = new LivroRepositorioCSV();
+            var livro = _repo.Todos.First(x => x.Id == id);
+
+            return context.Response.WriteAsync(livro.Detalhes());
+        }
+
+        private Task CadastroNovoLivro(HttpContext context)
+        {
+            var livro = new Livro()
+            {
+                Titulo = context.GetRouteValue("nome").ToString(),
+                Autor = context.GetRouteValue("autor").ToString()
+            };
+            var _repo = new LivroRepositorioCSV();
+            _repo.Incluir(livro);
+
+            return context.Response.WriteAsync("Livro adicionado com sucesso!");
+        }
+
         public Task Roteamento(HttpContext context)
         {
             var _repo = new LivroRepositorioCSV();
@@ -24,7 +66,6 @@ namespace Alura.ListaLeitura.App
                 {"/Livros/Lido", LivrosLidos },
             };
 
-            //comentário teste
             if (caminhosAtendidos.ContainsKey(context.Request.Path))
             {
                 var metodo = caminhosAtendidos[context.Request.Path];
@@ -47,13 +88,13 @@ namespace Alura.ListaLeitura.App
         public Task LivrosLendo(HttpContext context)
         {
             var _repo = new LivroRepositorioCSV();
-            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
         }
 
         public Task LivrosLidos(HttpContext context)
         {
             var _repo = new LivroRepositorioCSV();
-            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
         }
     }
 }
